1272789e21f5ada8638b4caa9ee78a6069dff37f
AshfaaqAhamed <ashfaaqahamed17@gmail.com>
2023-07-29 01:31:16 +0530

    minor change in login comp.

--- routes/auth.js
+++ routes/auth.js
@@ -11,12 +11,20 @@ router.post("/login", async (request, response) => {
   const { email, password } = request.body;
   if (!email || !password) return response.send(400);
   const userDB = await User.findOne({ email });
+  console.log("userDB -> LOGIN>> ", userDB);
+  // userDetails = [userDB.userName, userDB.email, userDB.role];
+  const userDetails = {
+    userName: userDB.userName,
+    email: userDB.email,
+    role: userDB.role,
+    userId: userDB._id,
+  };
   if (!userDB) return response.send(401);
   const isValid = comparePassword(password, userDB.password);
   if (isValid) {
     console.log("Authenticated Successfully!");
     // request.session.user = userDB;
-    return response.send(200);
+    return response.send(200, userDetails);
   } else {
     console.log("Failed to Authenticate");
     return response.send(401);
@@ -36,6 +44,11 @@ router.post("/register", async (request, response) => {
     const newUser = await User.create({ userName, password, email });
     response.send(201);
   }
+});
+
+router.get("/all", async (request, response) => {
+  const users = await User.find();
+  response.send(users);
 });
 
 module.exports = router;
